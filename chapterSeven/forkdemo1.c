c4e10b2c914b9dd4bd1d2ac1b568bbd82f16a2d4
scofieldWyq <794870409@qq.com>
2015-04-27 20:38:39 +0800

    complete how to build a process

---
+++ chapterSeven/forkdemo1.c
@@ -0,0 +1,23 @@
+/*
+ * forkdemo1.c
+ * shows how fork creates two processes. distinguishable
+ * by the different return values from fork()
+ */
+
+#include <stdio.h>
+
+main()
+{
+	int ret_from_fork, mypid;
+
+	mypid = getpid();		/* who am i ? */
+	printf("Before: my pid is %d\n", mypid);	/* tell the world */
+
+	ret_from_fork = fork();
+
+	sleep(1);
+	printf("After: my pid is %d, for() said %d\n",
+			getpid(), ret_from_fork);
+}
+
+
