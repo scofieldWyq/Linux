ebe8489881133ce5c7c0344d9f9084bc27d45a07
scofieldWyq <794870409@qq.com>
2015-04-28 11:29:44 +0800

    not completed simple shell

---
+++ chapterEight/splitline.c
@@ -0,0 +1,63 @@
+/*
+ * splitline.c - command reading and parsing functions for smsh
+ *
+ * char *next_cmd( char *prompt, FILE *fp) - get next command
+ * char **splitline(char *str); - parse a string
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "smsh.h"
+
+char *next_cmd( char *prompt, FILE *fp)
+/*
+ * purpose: read next command line from fp
+ * returns: dynamically allocated string holding command line
+ * errors: NULL at EOF( not really an error)
+ * 		calls fatal from emalloc()
+ * notes: allocates space in BUFSIZ chunks.
+ */
+{
+	char *buf;	/* the buffer */
+	int bufspace = 0;	/* total size */
+	int pos = 0;		/* current position */
+	int c;			/* input char */
+
+	printf("%s", prompt);
+	while( (c = getc(fp)) != EOF){
+
+		/* need space ? */
+		if( pos + 1 >= bufspace){	/* 1 for \0 */
+			if( bufspace == 0)	/* y: 1st time */
+				buf = emalloc(BUFSIZ);
+			else /* or expand */
+				buf = erealloc(buf, bufspace + BUFSIZ);
+
+			bufspace += BUFSIZ;	/* update size */
+		}
+
+		/* end of command ? */
+		if( c == '\n')
+			break;
+
+		/* no, ad to buffer */
+		buf[pos++] = c;
+	}
+
+	if( c == EOF && pos == 0)
+		return NULL;
+	buf[pos] = '\0';
+	return buf;
+}
+
+/*
+ * spliteline (parse a line into an array of strings)
+ */
+
+#define is_delim(x) ((x) == ' ' || (x) == '\t' )
+
+char **splitline(char *line);
+/*
+ * purpose: split a line into array of while - space separated tokens
+ * returns: a NULL - terminated array of pointers to copies of the
